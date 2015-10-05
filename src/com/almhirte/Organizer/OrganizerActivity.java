84a77f5d70f5a26046346745e74a86725fea44d3
almhirte <almhirte@yahoo.com>
2015-10-05 21:26:00 +0200

    first commit

---
+++ src/com/almhirte/Organizer/OrganizerActivity.java
@@ -0,0 +1,15 @@
+package com.almhirte.Organizer;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class OrganizerActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
